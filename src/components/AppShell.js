9159b0dc1facd20d87c6c7ccf9929aad09543e17
HANJIYEON <onedelay1@naver.com>
2022-03-07 23:34:43 +0900

    라우터(Router) 활용하기

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Drawer from "@material-ui/Drawer";
-import MenuItem from '@material-ui/core/MenuItem';
+import Drawer from '@material-ui/core/Drawer';
+//import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-u i/icons/Manu';
+//import MenuIcon from '@material-ui/icons/Manu';
+
 
 const styles = {
     root: {
@@ -27,16 +28,20 @@ class AppShell extends React.Component {
     render() {
         const { classes } = this.props;
         return (
-            <div classname={classes.root}>
-                <AppBar position="static">
-                    <IconButton className={classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
-                        <menuIcon/>
-                    </IconButton>
-                </AppBar>
-                <Drawer open={this.state.toggle}>
-                    <menuItem onClick={this.handleDrawerToggle}>Home</menuItem>
-                </Drawer>
-
+            <div>
+                <div className={classes.root}>
+                    <AppBar position="static">
+                        <IconButton className={classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
+                            <menuIcon/>
+                        </IconButton>
+                    </AppBar>
+                    <Drawer open={this.state.toggle}>
+                        <menuItem onClick={this.handleDrawerToggle}>Home</menuItem>
+                    </Drawer>
+                </div>
+                <div id="content" style={{margin: 'auto', marginTop: '20px'}}>
+                    {React.cloneElement(this.props.children)}
+                </div>
             </div>
         );
     }
