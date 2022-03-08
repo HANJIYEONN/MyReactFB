afa79c51bd5523b5f5ae2aba320266965868f3bf
HANJIYEON <onedelay1@naver.com>
2022-03-08 22:55:30 +0900

    라우터(Router) 활용하기 -> react-router-dom 안되는 오류 수정(버전 재설정)

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
+
 import {withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Drawer from '@material-ui/core/Drawer';
-//import MenuItem from '@material-ui/core/MenuItem';
+import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
-//import MenuIcon from '@material-ui/icons/Manu';
+import MenuIcon from '@material-ui/icons/Menu';
 
 
 const styles = {
@@ -13,8 +16,8 @@ const styles = {
     },
     menuButton: {
         marginRight: 'auto'
-    }
-}
+    },
+};
 
 
 class AppShell extends React.Component {
@@ -32,11 +35,25 @@ class AppShell extends React.Component {
                 <div className={classes.root}>
                     <AppBar position="static">
                         <IconButton className={classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
-                            <menuIcon/>
+                            <MenuIcon/>
                         </IconButton>
                     </AppBar>
                     <Drawer open={this.state.toggle}>
-                        <menuItem onClick={this.handleDrawerToggle}>Home</menuItem>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            <Link component={RouterLink} to="/">
+                                Home
+                            </Link>
+                        </MenuItem>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            <Link component={RouterLink} to="/texts">
+                                Texts
+                            </Link>
+                        </MenuItem>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            <Link component={RouterLink} to="/words">
+                                Words
+                            </Link>
+                        </MenuItem>
                     </Drawer>
                 </div>
                 <div id="content" style={{margin: 'auto', marginTop: '20px'}}>
