5f856f22e0f9bd24f624fa0c9076320db7de6794
HANJIYEON <onedelay1@naver.com>
2022-03-12 23:05:08 +0900

    커밋 오류 수정중

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -41,17 +41,17 @@ class AppShell extends React.Component {
                     <Drawer open={this.state.toggle}>
                         <MenuItem onClick={this.handleDrawerToggle}>
                             <Link component={RouterLink} to="/">
-                                홈 화면
+                                Home
                             </Link>
                         </MenuItem>
                         <MenuItem onClick={this.handleDrawerToggle}>
                             <Link component={RouterLink} to="/texts">
-                                텍스트 관리
+                                Texts
                             </Link>
                         </MenuItem>
                         <MenuItem onClick={this.handleDrawerToggle}>
                             <Link component={RouterLink} to="/words">
-                                단어 관리
+                                Words
                             </Link>
                         </MenuItem>
                     </Drawer>
