a3b7b5cabf24913b30076813bbb7f120865356db
HANJIYEON <onedelay1@naver.com>
2022-03-29 22:24:38 +0900

    복습 및 주석 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+//Webpack이란?
+//=모듈 번들링, html 파일에 들어가는 자바스크립트 파일들을 하나의 자바스크립트 파일로 만들어주는 방식
+
+
 'use strict'
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -14,7 +18,7 @@ module.exports = {
         rules: [{
             test: /\.js$/,
             include: path.resolve(__dirname, './src'),
-            loaders: 'babel-loader'
+            loaders: 'babel-loader' //최신 ES6버전을 구 버전인 ES5로 변환해준다. (인터넷 익스플로러11 지원)
         }, {
             test: /\.css$/,
             loader: "style-loader!css-loader"
