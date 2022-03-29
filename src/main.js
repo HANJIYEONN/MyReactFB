8ff9d271fbad81d796b570a6c4c04815515eb327
HANJIYEON <onedelay1@naver.com>
2022-03-29 22:24:28 +0900

    복습 및 주석 추가

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ const theme = unstable_createMuiStrictModeTheme ({
     }
 })
 
+//index.html 에서 작성한 app이라는 공간에 App이라는 컴퍼넌트를 그리고자 함 => components 폴더 안에 App.js 와 연결
 // ReactDOM.render(<App/>, document.getElementById('app'))..
 ReactDOM.render(<MuiThemeProvider theme={ theme }><App/></MuiThemeProvider>, document.getElementById('app'));
+
 
